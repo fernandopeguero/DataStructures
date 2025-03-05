de8568714ff7db772f60a33151616d1c5634c847
Fernando Peguero <fernandopeguerofs@gmail.com>
2025-03-04 23:42:36 -0500

    Update hash function

--- hashmap/hashmap.js
+++ hashmap/hashmap.js
@@ -1,10 +1,16 @@
-function hash() {
+function hash(key, arraylen) {
     let hashCode = 0;
 
     const primeNumber = 31;
-    for (let i = 0; i < key.length; i++) {
-        hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % primeNumber;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+        let char = key[i];
+        let value = char.charCodeAt(0) - 96;
+        hashCode = (hashCode * primeNumber + value) % arraylen;
     }
 
     return hashCode;
+}
+
+class hashMap {
+    constructor() {}
 }
