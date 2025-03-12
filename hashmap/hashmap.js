7bcd32bee38badf17e3f738adcf9d889aae1808c
Fernando Peguero <fernandopeguerofs@gmail.com>
2025-03-11 21:23:28 -0400

    Fix issue dividing by zero

--- hashmap/hashmap.js
+++ hashmap/hashmap.js
@@ -1,25 +1,35 @@
-function hash(key, arraylen) {
-    let hashCode = 0;
-
-    const primeNumber = 31;
-    for (let i = 0; i < Math.min(key.length, 100); i++) {
-        let char = key[i];
-        let value = char.charCodeAt(0) - 96;
-        hashCode = (hashCode * primeNumber + value) % arraylen;
+class hashMap {
+    constructor(size = 53) {
+        this.keyMap = new Array(size);
     }
 
-    return hashCode;
-}
+    hash(key, arraylen) {
+        let hashCode = 0;
 
-class hashMap {
-    constructor() {
-        this.list = [];
+        const primeNumber = 31;
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i];
+            let value = char.charCodeAt(0) - 96;
+            hashCode = (hashCode * primeNumber + value) % (arraylen || 2);
+        }
+
+        return hashCode;
     }
 
     set(key, value) {
         const hashValue = hash(key, this.list.length);
 
-        this.list[hashValue] = value;
+        this.list.splice(hashValue, 0, value);
+    }
+
+    get(key) {
+        const hashKey = hash(key, this.list.length);
+
+        if (this.list[hashKey]) {
+            return this.list[hashKey];
+        }
+
+        return null;
     }
 }
 
