3bbdb49a41000f780415caad513d5c7f7375d8a0
Fernando Peguero <fernandopeguerofs@gmail.com>
2025-03-13 23:41:43 -0400

    Add logic to get a 2 dimensinal array of key/values

--- hashmap/hashmap.js
+++ hashmap/hashmap.js
@@ -73,6 +73,18 @@ class hashMap {
 
         return list;
     }
+
+    entries() {
+        const keys = this.keys();
+
+        const list = [];
+
+        for (let i = 0; i < keys.length; i++) {
+            list.push([keys[i], this.keyMap[keys[i]]]);
+        }
+
+        return list;
+    }
 }
 
 const map = new hashMap();
